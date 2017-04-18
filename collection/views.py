71ceeed50c1bc1ef7e4ca5293f71451261f1e24f
Caleb Sabatini <calebsabatini@gmail.com>
2017-04-18 18:06:34 -0500

    add tag to urls with placeholder view

--- collection/views.py
+++ collection/views.py
@@ -5,3 +5,7 @@ import services
 def index(request):
     tag_list = services.get_tags()
     return render(request, 'index.html', tag_list)
+
+
+def tag(request, tag_id):
+    return render(request, 'tag.html', {'tag_id': tag_id})
