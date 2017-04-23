6452a5c275bc96a57e6079d8c458b866adede98f
Caleb Sabatini <calebsabatini@gmail.com>
2017-04-23 15:30:28 -0500

    tox config, pytest dependencies

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages, Command
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*/*.pyc '
+                  './*/*/*.pyc ./*.tgz ./*.egg-info ./*/__pycache__ ./*/*/__pycache__ ./.eggs ./.cache')
+
+
+setup(name='django-trumptweets',
+      version='0.0.0',
+      description='Simple django project to consume the TrumpTweets API',
+      url='https://github.com/csabatini/django-trumptweets',
+      author='Caleb Sabatini',
+      author_email='calebsabatini@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+          'Django==1.8.11',
+          'requests==2.9.1'
+      ],
+      setup_requires=['pytest-runner'],
+      test_suite='pytest',
+      tests_require=[
+          'pytest==3.0.7',
+          'pytest-django==2.9.1',
+          'mockito==1.0.11'
+      ],
+      zip_safe=False,
+      cmdclass={
+          'clean': CleanCommand
+      })
